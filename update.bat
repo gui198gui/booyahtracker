271f5ba42a179dc0b2b9c1960d8722e3ccc38887
gui198gui <veteranxx3@gmail.com>
2026-04-05 23:28:44 +0100

    Ranking

--- update.bat
+++ update.bat
@@ -3,8 +3,12 @@ echo 🚀 A iniciar o motor de busca da Bungie...
 cd raid
 node tracker_completo.js
 
+echo.
 echo 📦 A copiar os novos dados para o site...
+:: O comando abaixo sobe uma pasta (..) e entra na pasta do site
 copy ranking.json ..\booyah-tracker\src\app\ranking.json /Y
 
-echo ✅ Concluido! O teu site ja esta atualizado.
+echo.
+echo ✅ Concluido! Dados copiados para o dashboard.
+echo 💡 Agora vai ao GitHub Desktop, faz Commit e Push para atualizar o site online!
 pause
